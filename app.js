94baafedceff0dc0a578feacd61409571fafceb5
Max Sharma <maxysharma98@gmail.com>
2018-01-20 18:41:25 -0800

    added problem4

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function sumAndMultiply(a, b, c) {
   console.log(array3);
   return array3;
 
- 
+
 
 
   //eslint-disable-line
@@ -85,12 +85,21 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) {
+  var total=0;
+  for (var i = 0; i < testArray.length; i++) {
+    var num = testArray[i];
+    total = total + num;
+  }
+  var result4 = testArray + ' was passed in as an array of numbers, and ' + total + ' is their sum';
+  var array4 = [total,result4];
+  console.log(array4);
+  return array4;
+
   //eslint-disable-line
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
